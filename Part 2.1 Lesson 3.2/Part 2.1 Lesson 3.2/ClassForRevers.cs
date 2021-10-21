5efa422a6b736e1d839d0b15203a6d28dc315404
distorition <85959781+distorition@users.noreply.github.com>
2021-10-21 12:21:35 +0300

    Add files via upload

---
+++ Part 2.1 Lesson 3.2/Part 2.1 Lesson 3.2/ClassForRevers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2._1_Lesson_3._2
+{
+   public  class ClassForRevers
+    {
+        public void Revers(string str)
+        {
+            char[] vars = str.ToCharArray();
+            Array.Reverse(vars);
+            Console.WriteLine();
+            string outp = new string(vars);
+            Console.WriteLine(outp);
+        }
+    }
+}
---
+++ Part 2.1 Lesson 3.2/Part 2.1 Lesson 3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Part_2._1_Lesson_3._2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("напишите слово");
+            string words = Console.ReadLine();
+            ClassForRevers classFor = new ClassForRevers();
+            classFor.Revers(words);
+            
+
+        }
+    }
+}
